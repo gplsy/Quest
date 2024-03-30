7a880119d98958ee385882ac39db0f02de3ea89e
gplsy <keliatanbego@gmail.com>
2024-03-31 02:40:44 +0700

    Create price-gplsy.lua

---
+++ price-gplsy.lua
@@ -0,0 +1,12 @@
+-- PID: UgkdOEYfUDQWpHRKWADh7ZVHsB_gPDNeWLT-mJLjmqU        
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"     
+Handlers.add("gplsy", -- change this (username)                Handlers.utils.hasMatchingTag("Action", "gplsy-github"), -- change this too (tagname)                                 function(msg)
+        local token = msg.Tags.Token                               local currency = msg.Tags.Currency
+        local url = "https://api.coingecko.com/api/v3/simple/price?ids=" .. token .. "&vs_currencies=" .. currency            ao.send({
+            Target = _0RBIT,
+            Action = "Get-Real-Data",
+            Url = url
+        })
+        Handlers.utils.reply("PriceFetch")(msg)
+    end
+)
